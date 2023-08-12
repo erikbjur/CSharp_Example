3f31e84388ffa943042b97489185264f38082834
Erik Bjur <erik@bjurengineering.com>
2023-08-11 18:11:01 -0700

    Initial Commit

---
+++ Hello World/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Hello_World;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("Press enter to count to 10");
+
+        Console.ReadKey();
+
+        for( int i = 0; i < 10+1; i++) {
+            Console.WriteLine(i);
+        }
+    }
+}
